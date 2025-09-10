72854c04c9a4d444aaf2b071aeb01f68a6684ad2
Snake16547 <32037245+Snake16547@users.noreply.github.com>
2025-09-10 07:32:53 -0400

    Update debian-setup.sh

--- debian-setup.sh
+++ debian-setup.sh
@@ -14,11 +14,11 @@ BACKUP_DIR=”/root/debian-setup-backups”
 
 # Color codes for output
 
-RED=’\033[0;31m’
-GREEN=’\033[0;32m’
-YELLOW=’\033[1;33m’
-BLUE=’\033[1;34m’
-NC=’\033[0m’ # No Color
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[1;34m'
+NC='\033[0m' # No Color
 
 # Logging function
 
